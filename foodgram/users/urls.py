60a015b83ffce395f484507f46bb5a2e832e7cd1
makeitokay <andrey.mu11@yandex.ru>
2020-09-02 22:25:46 +0500

    Регистрация

--- foodgram/users/urls.py
+++ foodgram/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('login/', views.FoodgramLoginView.as_view()),
+    path('signup/', views.RegistrationView.as_view()),
     path('', include('django.contrib.auth.urls')),
 ]
--- foodgram/users/views.py
+++ foodgram/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.views.generic import FormView
 
-# Create your views here.
+from .forms import FoodgramUserCreationForm
+
+
+class FoodgramLoginView(LoginView):
+    redirect_authenticated_user = True
+
+
+class RegistrationView(FormView):
+    form_class = FoodgramUserCreationForm
+    template_name = 'registration/signup.html'
+    redirect_authenticated_user = True
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        return redirect(self.success_url)
