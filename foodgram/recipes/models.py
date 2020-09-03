066375b686ff60212ea316e007a0e73d66025983
makeitokay <andrey.mu11@yandex.ru>
2020-09-03 23:27:01 +0500

    Заготовка моделей: Recipe, Ingredient, Tag, RecipeIngredients

--- foodgram/recipes/models.py
+++ foodgram/recipes/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from foodgram.recipes.utils import russian_slugify
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=40)
+    unit = models.CharField(max_length=10)
+
+
+class RecipeIngredients(models.Model):
+    amount = models.DecimalField()
+    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+
+
+class Recipe(models.Model):
+    TAG_CHOICES = (
+        (0, 'Завтрак'),
+        (1, 'Обед'),
+        (2, 'Ужин')
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="recipes",
+    )
+    name = models.CharField(max_length=60)
+    photo = models.ImageField(upload_to='recipes/')
+    description = models.TextField()
+    ingredients = models.ManyToManyField('Ingredient', related_name='recipes',
+                                         through='RecipeIngredients')
+    tags = models.ManyToManyField('Tag', related_name='recipes')
+    cooking_time = models.IntegerField()
+    slug = models.SlugField(unique=True, null=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = russian_slugify(self.name)
+        super().save(*args, **kwargs)
---
+++ foodgram/recipes/utils.py
@@ -0,0 +1,39 @@
+from django.utils.text import slugify
+
+russian_alphabet = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ё": "yo",
+    "ж": "zh",
+    "з": "z",
+    "и": "i",
+    "й": "y",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "х": "kh",
+    "ц": "ts",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "sch",
+    "ы": "y",
+    "э": "e",
+    "ю": "yu",
+    "я": "ya",
+}
+
+
+def russian_slugify(text):
+    return slugify("".join(russian_alphabet.get(s, s) for s in text.lower()))
