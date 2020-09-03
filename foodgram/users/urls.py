e96c8caaece43253caa7d96d55e53ed2fdf5d890
makeitokay <andrey.mu11@yandex.ru>
2020-09-03 21:03:03 +0500

    Рефакторинг шаблонов

--- foodgram/users/urls.py
+++ foodgram/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.FoodgramLoginView.as_view()),
-    path('signup/', views.RegistrationView.as_view()),
+    path('login/', views.FoodgramLoginView.as_view(), name='login'),
+    path('signup/', views.RegistrationView.as_view(), name='signup'),
     path('', include('django.contrib.auth.urls')),
 ]
