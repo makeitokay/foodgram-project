3cbf275ae13a8fa02f6500ff2dd7c468c63cfc29
makeitokay <andrey.mu11@yandex.ru>
2020-09-10 00:11:05 +0500

    Страница автора

--- foodgram/accounts/urls.py
+++ foodgram/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('favorites/', views.FavoriteRecipeListView.as_view(), name='favorite-list'),
+    path('<int:pk>/', views.AuthorRecipeListView.as_view(), name='author-recipe-list'),
 ]
--- foodgram/accounts/views.py
+++ foodgram/accounts/views.py
@@ -25,3 +25,30 @@ class FavoriteRecipeListView(LoginRequiredMixin, ListView):
             queryset = Recipe.objects.all()
         queryset = queryset.filter(pk__in=favorites)
         return queryset.order_by('-id').prefetch_related('author', 'tags').all()
+
+
+class AuthorRecipeListView(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    template_name = 'authorRecipe.html'
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            favorites = [e[0] for e in Favorite.objects.filter(user=self.request.user).values_list('recipe')]
+            context['favorites'] = favorites
+
+        context['tags'] = self.request.GET.get('tags', '')
+
+        return context
+
+    def get_queryset(self):
+        tags = self.request.GET.get('tags', '')
+        if tags:
+            queryset = Recipe.objects.filter(tags__name__in=tags.split(',')).distinct()
+        else:
+            queryset = Recipe.objects.all()
+        queryset = queryset.filter(author=self.kwargs.get('pk'))
+        return queryset.order_by('-id').prefetch_related('author', 'tags').all()
